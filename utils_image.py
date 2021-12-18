aac075c10b2b93c792c4ccd52e2d09c2e625f47f
Cory Flavin <cflavin_imac@Corys-iMac.local>
2021-12-18 15:39:48 -0700

    updating cell selection

--- utils_image.py
+++ utils_image.py
@@ -8,55 +8,87 @@ import numpy as np
 from scipy import ndimage as ndi
 from skimage import (color, feature, filters, measure, morphology, segmentation, util)
 
-threshold = 1000
 
+#initialize variables
+threshold = 1000
+intensity_limit = 3000
+
+
+#load in file, create mask and display image
 nd2 = nd2reader.Nd2("./03_A3_40x.nd2")
 npfile = np.array(nd2[1])
 plt.imshow(npfile)
-plt.savefig('image.png')
-masknp = npfile
+plt.savefig('image1.png')
+phase_image = npfile
 
-mask = masknp >= threshold
-mask2 = masknp < threshold
-masknp[mask] = 1
-masknp[mask2] = 0
-plt.imshow(masknp)
+
+#extract pixel microns from nd2 file
+with ND2Reader("./03_A3_40x.nd2") as images:
+    pixel_microns = images.metadata['pixel_microns']
+print("Pixel micron count: %s" %pixel_microns)
+
+
+#display phase image
+plt.imshow(phase_image)
+fig, ax = plt.subplots(figsize=(5, 5))
+qcs = ax.contour(phase_image, origin='image')
 plt.savefig('image2.png')
 
-plt.imshow(masknp)
-fig, ax = plt.subplots(figsize=(5, 5))
-qcs = ax.contour(masknp, origin='image')
-plt.show()
+
+#display histogram of intensity 
+hist, bins = skimage.exposure.histogram(phase_image)
+plt.plot(bins, hist, linewidth=1)
+plt.xlim([0,intensity_limit])
+plt.xlabel('pixel value (a.u.)')
+plt.ylabel('counts')
 plt.savefig('image3.png')
 
-print(qcs.levels)
-print(len(seg) for seg in qcs.allsegs)
 
-thresholds = filters.threshold_multiotsu(masknp, classes=2)
-regions = np.digitize(masknp, bins=thresholds)
+#setup threshold and display
+threshold_image = phase_image < threshold
+plt.clf()
+plt.set_cmap('Blues')
+plt.imshow(threshold_image)
+plt.savefig('image4')
 
-fig, ax = plt.subplots(ncols=2, figsize=(10, 5))
-ax[0].imshow(masknp)
-ax[0].set_title('Original')
-ax[0].axis('off')
-ax[1].imshow(regions)
-ax[1].set_title('Multi-Otsu thresholding')
-ax[1].axis('off')
-plt.show()
-plt.savefig('image4.png')
 
-cells = masknp > thresholds[0]
+#find cells
+cells = phase_image > threshold_image[0]
 labeled_cells = measure.label(cells)
-im_lab, num_obj = masknp.measure.label(thresholds, return_num=True)
-print(num_obj)
-
-plt.imshow(labeled_cells)
-plt.show()
-plt.savefig('image5.png')
-
-print(labeled_cells)
+im_lab, num_obj = skimage.measure.label(threshold_image, return_num=True)
+print("Number of objects found: %s" %num_obj)
 
 
+bac_1 = im_lab == 2
+cell_pix = np.sum(bac_1)
+cell_area = cell_pix * pixel_microns**2
+print("Pixel count: %s" %cell_pix)
+print("Cell Area: %s" %cell_area)
+
+
+#calculate cell areas
+areas = np.zeros(num_obj)
+for i in range(num_obj):
+    cell = (im_lab == i + 1)
+    areas[i] = np.sum(cell) * pixel_microns**2
+#print(areas)
+    
+    
+
+
+quit()
 
 ##Various Attempts
 #a = nd2.select(channels="Cy5",z_levels=(0))
+#print(qcs.levels)
+#print(len(seg) for seg in qcs.allsegs)
+#threshold_image = filters.threshold_multiotsu(masknp_image, classes=2)
+#regions = np.digitize(masknp_image, bins=threshold_image)
+#fig, ax = plt.subplots(ncols=2, figsize=(10, 5))
+#ax[0].imshow(mask_image)
+#ax[0].set_title('Original')
+#ax[0].axis('off')
+#ax[1].imshow(regions)
+#ax[1].set_title('Multi-Otsu thresholding')
+#ax[1].axis('off')
+#plt.savefig('image4.png')

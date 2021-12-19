669f39a3aabbdd91e0ba79dbc946b27555d9bcfa
Cory Flavin <cflavin_imac@Corys-iMac.local>
2021-12-19 12:17:00 -0700

    cell labeling and fluorescence intensity complete

--- utils_image.py
+++ utils_image.py
@@ -4,22 +4,33 @@ import skimage
 from skimage import data, io, filters
 import matplotlib.pyplot as plt
 import numpy.ma as ma
+import sys
 import numpy as np
 from scipy import ndimage as ndi
 from skimage import (color, feature, filters, measure, morphology, segmentation, util)
+import csv
+import imageio
+import pandas as pd
 
 
 #initialize variables
-threshold = 1000
+nd2_file = "./03_A3_40x.nd2"
+channel_cells = 1
+channel_fluorescence = 0
+threshold_val = 1500
 intensity_limit = 3000
+cellarea_lower = 10
+cellarea_upper = 40
 
 
-#load in file, create mask and display image
-nd2 = nd2reader.Nd2("./03_A3_40x.nd2")
-npfile = np.array(nd2[1])
-plt.imshow(npfile)
+#load in files and display image
+nd2_image = nd2reader.Nd2("./03_A3_40x.nd2")
+npfile_phase = np.array(nd2_image[1])
+npfile_fluorescent = np.array(nd2_image[0])
+plt.imshow(npfile_phase)
 plt.savefig('image1.png')
-phase_image = npfile
+phase_image = npfile_phase
+fluorescent_image = npfile_fluorescent
 
 
 #extract pixel microns from nd2 file
@@ -36,6 +47,7 @@ plt.savefig('image2.png')
 
 
 #display histogram of intensity 
+plt.clf()
 hist, bins = skimage.exposure.histogram(phase_image)
 plt.plot(bins, hist, linewidth=1)
 plt.xlim([0,intensity_limit])
@@ -45,23 +57,19 @@ plt.savefig('image3.png')
 
 
 #setup threshold and display
-threshold_image = phase_image < threshold
+threshold_image = phase_image > threshold_val
 plt.clf()
 plt.set_cmap('Blues')
 plt.imshow(threshold_image)
 plt.savefig('image4')
 
 
-#find cells
-cells = phase_image > threshold_image[0]
-labeled_cells = measure.label(cells)
+#find cells, area. 
+np.set_printoptions(threshold=np.inf)
 im_lab, num_obj = skimage.measure.label(threshold_image, return_num=True)
-print("Number of objects found: %s" %num_obj)
-
-
-bac_1 = im_lab == 2
-cell_pix = np.sum(bac_1)
+cell_pix = np.count_nonzero(im_lab == 3)
 cell_area = cell_pix * pixel_microns**2
+print("Number of objects found: %s" %num_obj)
 print("Pixel count: %s" %cell_pix)
 print("Cell Area: %s" %cell_area)
 
@@ -69,16 +77,72 @@ print("Cell Area: %s" %cell_area)
 #calculate cell areas
 areas = np.zeros(num_obj)
 for i in range(num_obj):
-    cell = (im_lab == i + 1)
-    areas[i] = np.sum(cell) * pixel_microns**2
-#print(areas)
+    cell = (im_lab == (i+1))
+    areas[i] = np.count_nonzero(cell) * pixel_microns**2
+
     
-    
+#determine approved cells based on area
+approved_cells = np.zeros_like(threshold_image)
+for i in range(num_obj):
+    cell = (im_lab == (i+1))
+    cell_area = np.count_nonzero(cell) * pixel_microns**2
+    if (cell_area > cellarea_lower) & (cell_area < cellarea_upper):
+        approved_cells += cell
+
+#show approved cells
+approved_lab, num_obj = skimage.measure.label(approved_cells, return_num=True)
+print("Approved cells: %s" %num_obj)
+plt.clf()
+plt.imshow(approved_lab)
+plt.savefig('image5')
 
 
-quit()
+#load fluorescent image
+plt.clf()
+plt.set_cmap('Greens')
+plt.imshow(fluorescent_image)
+plt.savefig('image6')
+
+
+#find intensity of one cell
+cell_id = 5
+cell = (approved_lab == cell_id)
+cell_fluorescence = cell * fluorescent_image
+total_int = np.sum(cell_fluorescence)
+print('Cell 5 intensity: %s' % total_int)
+plt.clf()
+plt.set_cmap('Greens')
+plt.imshow(cell_fluorescence)
+plt.savefig('image7')
+
+
+#Measure cell fluorescence
+tot_intensities = np.zeros(num_obj) 
+mean_intensities = np.zeros(num_obj) 
+cell_areas = np.zeros(num_obj)
+for i in range(num_obj):
+    cell = (approved_lab == i+1)
+    cell_areas[i] = np.sum(cell) * pixel_microns**2
+    fluorescent_intensity = cell * fluorescent_image
+    tot_intensities[i] = np.sum(fluorescent_intensity)
+    mean_intensities[i] = np.sum(fluorescent_intensity) / np.count_nonzero(cell)
+plt.clf()
+plt.hist(tot_intensities, bins=10)
+plt.xlabel('total intensity (a.u.)')
+plt.ylabel('count')
+plt.savefig('image8')
+plt.clf()
+print(mean_intensities)
+    
+
+#generate report of images
+
+df = pd.DataFrame(mean_intensities)
+df.to_csv('outputdata.csv')
 
 ##Various Attempts
+#cells = phase_image > threshold_image[0]
+#labeled_cells = measure.label(cells)
 #a = nd2.select(channels="Cy5",z_levels=(0))
 #print(qcs.levels)
 #print(len(seg) for seg in qcs.allsegs)
